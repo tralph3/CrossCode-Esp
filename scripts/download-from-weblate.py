9c6fc39ae04ab50f6f99ef1ba1fa77a2319c06f7
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-06-07 23:55:42 +0300

    polish the Weblate API downloader and use it (isntead of Nginx)
    This is because the download endpoint automatically flushes all changes
    made to the file without the need for comitting by hand, even when done
    anonymously (possible attack vector???) thus removing the need for
    giving out the VCS control permission to testers. Also the change time
    detection depends on my patches to Weblate, gonna publish them soon.
    Also had to extend the list of gzipped MIME types on the nginx side
    because Weblate serves the `.po`s with the (correct)
    `text/x-gettext-catalog` MIME type. Also took an opportunity to fix
    linting errors.
    
    [1]: <https://github.com/dmitmel/crosscode-weblate/blob/22fa3c9ce31c1e0e87fa44bb5a3cef8856d2e5a2/weblate/utils/views.py#L274-L275>

--- scripts/download-from-weblate.py
+++ scripts/download-from-weblate.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
-import urllib.request
-from http.client import HTTPResponse
-from contextlib import contextmanager
 import gzip
-import json
-from multiprocessing.pool import ThreadPool
-from pathlib import Path
-from typing import Generator, NamedTuple, Optional, Any, List, Dict, Tuple
-from abc import ABCMeta, abstractmethod
-from datetime import datetime, timezone
-from email.utils import parsedate_to_datetime
 import io
-import subprocess
+import json
 import os
 import shutil
-
+import subprocess
+import urllib.request
+from abc import ABCMeta, abstractmethod
+from contextlib import contextmanager
+from datetime import datetime, timezone
+from email.utils import parsedate_to_datetime as parse_rfc2822_date
+from http.client import HTTPResponse
+from multiprocessing.pool import ThreadPool
+from pathlib import Path
+from typing import Any, Dict, Generator, List, NamedTuple, Optional, Tuple
 
 PROJECT_ORIGINAL_LOCALE = "en_US"
-PROJECT_ORIGINAL_LOCALE_SHORT = "en"
+PROJECT_ORIGINAL_LOCALE_WEBLATE = "en"
 PROJECT_LOCALE = "es_ES"
-PROJECT_LOCALE_SHORT = "es"
+PROJECT_LOCALE_WEBLATE = "es"
 PROJECT_TARGET_GAME_VERSION = "1.4.1-2"
 
 CROSSLOCALE_BIN_NAME = "crosslocale"
@@ -38,14 +37,16 @@ NETWORK_THREADS = 10
 def http_request(url: str) -> Generator[Tuple[HTTPResponse, io.BufferedIOBase], None, None]:
   print(f"fetching {url}")
   req = urllib.request.Request(url)
-  req.add_header('Accept-Encoding', 'gzip')
+  req.add_header("Accept-Encoding", "gzip")
 
+  res: object
   with urllib.request.urlopen(req, timeout=NETWORK_TIMEOUT) as res:
-    assert isinstance(res, HTTPResponse)
+    if not isinstance(res, HTTPResponse):
+      raise Exception("Not an HTTP request")
     reader: io.BufferedIOBase = res
 
     # <https://github.com/kurtmckee/feedparser/blob/727ee7f08f77d8f0a0f085ec3dfbc58e09f69a4b/feedparser/http.py#L166-L188>
-    content_encoding = res.getheader('content-encoding')
+    content_encoding = res.getheader("content-encoding")
     if content_encoding == "gzip":
       reader = gzip.GzipFile(fileobj=res)
 
@@ -64,7 +65,7 @@ class ComponentDownloader(metaclass=ABCMeta):
     raise NotImplementedError()
 
   @abstractmethod
-  def fetch_component(self, component: ComponentMeta):
+  def fetch_component(self, component: ComponentMeta) -> None:
     raise NotImplementedError()
 
 
@@ -73,18 +74,24 @@ class WeblateApiComponentDownloader(ComponentDownloader):
 
   def fetch_list(self) -> List[ComponentMeta]:
     components: List[ComponentMeta] = []
-    next_api_url = f"{self.HOST}/api/projects/crosscode/components/"
+    next_api_url = f"{self.HOST}/api/projects/crosscode/statistics/{PROJECT_LOCALE_WEBLATE}/"
     while next_api_url is not None:
-      with http_request(next_api_url) as (_response, response_reader):
+      with http_request(next_api_url) as (_, response_reader):
         api_response = json.load(response_reader)
         next_api_url = api_response["next"]
-        for component in api_response["results"]:
-          components.append(ComponentMeta(id=component["slug"], modification_timestamp=None))
+        for stats_obj in api_response["results"]:
+          mtime: Optional[datetime] = None
+          mtime_str: Optional[str] = stats_obj["last_change"]
+          if mtime_str is not None:
+            mtime = datetime.fromisoformat(
+              mtime_str[:-1] + "+00:00" if mtime_str.endswith("Z") else mtime_str
+            )
+          components.append(ComponentMeta(id=stats_obj["component"], modification_timestamp=mtime))
     return components
 
   def fetch_component(self, component: ComponentMeta) -> None:
     download_url = f"{self.HOST}/download/crosscode/{component.id}/{PROJECT_LOCALE}"
-    with http_request(download_url) as (_response, response_reader):
+    with http_request(download_url) as (_, response_reader):
       with open(DOWNLOADS_DIR / f"{component.id}.po", "wb") as output_file:
         shutil.copyfileobj(response_reader, output_file)
 
@@ -95,16 +102,16 @@ class NginxApiComponentDownloader(ComponentDownloader):
   def fetch_list(self) -> List[ComponentMeta]:
     components: List[ComponentMeta] = []
     api_url = f"{self.HOST}/__json__/~weblate/download/crosscode/{PROJECT_LOCALE}/components/"
-    with http_request(api_url) as (_response, response_reader):
+    with http_request(api_url) as (_, response_reader):
       for file_meta in json.load(response_reader):
         if file_meta["type"] == "file" and file_meta["name"].endswith(".po"):
-          mtime = parsedate_to_datetime(file_meta["mtime"])
+          mtime = parse_rfc2822_date(file_meta["mtime"])
           components.append(ComponentMeta(id=file_meta["name"][:-3], modification_timestamp=mtime))
     return components
 
   def fetch_component(self, component: ComponentMeta) -> None:
     download_url = f"{self.HOST}/__json__/~weblate/download/crosscode/{PROJECT_LOCALE}/components/{component.id}.po"
-    with http_request(download_url) as (_response, response_reader):
+    with http_request(download_url) as (_, response_reader):
       with open(DOWNLOADS_DIR / f"{component.id}.po", "wb") as output_file:
         shutil.copyfileobj(response_reader, output_file)
 
@@ -120,24 +127,51 @@ def main() -> None:
       downloads_state_json = json.load(file)
       if downloads_state_json["version"] == 1:
         for c_id, c_data in downloads_state_json["data"].items():
+          c_mtime: Optional[float] = c_data["mtime"]
           downloads_state[c_id] = ComponentMeta(
-            c_id, datetime.fromtimestamp(c_data["mtime"], timezone.utc)
+            id=c_id,
+            modification_timestamp=datetime.fromtimestamp(c_mtime, timezone.utc)
+            if c_mtime is not None else None,
           )
   except FileNotFoundError:
     pass
 
-  downloader: ComponentDownloader = NginxApiComponentDownloader()
+  downloader: ComponentDownloader = WeblateApiComponentDownloader()
   print("==> downloading the list of components")
   remote_components_list = downloader.fetch_list()
 
   components_to_fetch_list: List[ComponentMeta] = []
   for remote_meta in remote_components_list:
     local_meta = downloads_state.get(remote_meta.id)
-    if (
-      local_meta is None or remote_meta.modification_timestamp is None or
-      local_meta.modification_timestamp is None or
-      remote_meta.modification_timestamp > local_meta.modification_timestamp
-    ):
+    should_fetch: bool
+
+    if local_meta is None:
+      # We don't have this component download, it has probably been created
+      should_fetch = True
+    else:
+      # Notice that missing timestamps mean that the component has never been
+      # modified so far
+      remote_mtime = remote_meta.modification_timestamp
+      local_mtime = local_meta.modification_timestamp
+
+      if remote_mtime is None and local_mtime is None:
+        # We have downloaded an empty component and there are still no changes
+        # affecting it
+        should_fetch = False
+      elif remote_mtime is not None and local_mtime is None:
+        # Got the first ever changes!
+        should_fetch = True
+      elif remote_mtime is None and local_mtime is not None:
+        # We have already downloaded some changed version of the component, but
+        # it has probably been reset to an empty state since.
+        should_fetch = True
+      elif remote_mtime is not None and local_mtime is not None:
+        # The sane and normal code path.
+        should_fetch = remote_mtime > local_mtime
+      else:
+        raise Exception("unreachable")
+
+    if should_fetch:
       components_to_fetch_list.append(remote_meta)
 
   with ThreadPool(NETWORK_THREADS) as pool:
@@ -155,7 +189,7 @@ def main() -> None:
     print(f"==> downloading the scan database for v{PROJECT_TARGET_GAME_VERSION}")
 
     download_url = f"https://raw.githubusercontent.com/dmitmel/crosslocale-scans/main/scan-{PROJECT_TARGET_GAME_VERSION}.json"
-    with http_request(download_url) as (_response, response_reader):
+    with http_request(download_url) as (_, response_reader):
       with open(CROSSLOCALE_SCAN_FILE, "wb") as output_file:
         shutil.copyfileobj(response_reader, output_file)
 
@@ -192,7 +226,9 @@ def main() -> None:
       "version": 1,
       "data": {
         component_meta.id: {
-          "mtime": component_meta.modification_timestamp.timestamp(),
+          "mtime":
+            component_meta.modification_timestamp.timestamp()
+            if component_meta.modification_timestamp is not None else None,
         } for component_meta in downloads_state.values()
       },
     }
