d9d0f800b1924e1540ddba7e036587fbf01264f3
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-02-08 23:44:02 +0200

    remove the quest dialog patch that has been moved into enhanced-ui

--- src/prestart.js
+++ src/prestart.js
@@ -2,7 +2,6 @@ import './version-display.js';
 import './traders-list.js';
 import './level-up.js';
 import './tab-buttons.js';
-import './quest-dialog.js';
 import './quest-hub.js';
 import './equipment-menu.js';
 import './shop-menu.js';
