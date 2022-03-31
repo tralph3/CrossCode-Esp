b4d6d1bbba65d49c36c0d973e77047b094b62524
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-03-31 11:55:40 +0300

    use the generalized tab button patches from enhanced-ui

--- src/prestart.js
+++ src/prestart.js
@@ -1,7 +1,6 @@
 import './version-display.js';
 import './traders-list.js';
 import './level-up.js';
-import './tab-buttons.js';
 import './quest-hub.js';
 import './equipment-menu.js';
 import './shop-menu.js';
@@ -12,4 +11,12 @@ import './hud.js';
 sc.esp.addLocaleSpecificPatch(() => {
   sc.QuickBorderArrowLevelBox.prototype.UI2_DRAW_LEVEL_LABEL_AS_TEXT_BLOCK = true;
   sc.SocialEntryButton.prototype.UI2_DRAW_STATUS_AS_TEXT_BLOCK = true;
+
+  sc.ArenaCupList.prototype.UI2_INCREASE_TAB_BUTTON_WIDTH = 135;
+  sc.BotanicsListBox.prototype.UI2_INCREASE_TAB_BUTTON_WIDTH = 160;
+  sc.OptionsTabBox.prototype.UI2_INCREASE_TAB_BUTTON_WIDTH = 105;
+  sc.QuestHubList.prototype.UI2_INCREASE_TAB_BUTTON_WIDTH = 105;
+  sc.QuestListBox.prototype.UI2_INCREASE_TAB_BUTTON_WIDTH = 100;
+  sc.SocialList.prototype.UI2_INCREASE_TAB_BUTTON_WIDTH = 95;
+  sc.TradersListBox.prototype.UI2_INCREASE_TAB_BUTTON_WIDTH = 200;
 });
