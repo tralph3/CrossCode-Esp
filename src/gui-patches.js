e06d1ef48e09caf854cb68dd2d6f0d7020488432
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-12-21 18:59:21 +0100

    Revert "translate the "HP" and "SP" labels in the HUD"
    This reverts commit 9b5ddf8803b473e875ae71215b231d2d3a33cf5e.

--- src/gui-patches.js
+++ src/gui-patches.js
@@ -100,22 +100,6 @@ localizeMe.register_locale_chosen(() => {
     },
   });
 
-  sc.HpHudGui.inject({
-    patchedGfx: new ig.Image('media/gui/status-gui.es_ES.png'),
-    updateDrawables(renderer) {
-      this.parent(renderer);
-      renderer.addGfx(this.patchedGfx, 7, 0, 1, 20, 12, 7);
-    },
-  });
-
-  sc.SpHudGui.inject({
-    patchedGfx: new ig.Image('media/gui/status-gui.es_ES.png'),
-    updateDrawables(renderer) {
-      this.parent(renderer);
-      renderer.addGfx(this.patchedGfx, 7, 0, 1, 28, 12, 7);
-    },
-  });
-
   sc.QuestDetailsView.inject({
     init(...args) {
       this.parent(...args);
